f50ab3c453e0579dfb0c5d41346804f7c1626508
Il-Young Jeong <finejuly@gmail.com>
2018-12-26 14:52:02 +0000

    init

---
+++ examples.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from keras.layers import Input
+from keras.models import Model
+
+from kapre2keras.melspectrogram import Melspectrogram
+
+sr = 44100
+n_ch = 2
+n_sample = 44100
+input_shape = (n_ch, n_sample)
+
+inputs = Input(shape=(n_ch,None))
+
+outputs = Melspectrogram(n_dft=512, n_hop=256, input_shape=input_shape, padding='same', sr=sr, n_mels=128, fmin=0.0, fmax=sr/2, power_melgram=1.0, return_decibel_melgram=False, trainable_fb=False, trainable_kernel=False)(inputs)
+
+model = Model(inputs=inputs, outputs=outputs)
+model.summary()
+
+
+x = np.random.random((1,n_ch,n_sample))
+mel = model.predict(x)
---
+++ kapre2keras/melspectrogram.py
@@ -0,0 +1,69 @@
+from keras.models import Sequential, Model
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Reshape, Dense, Permute, Lambda
+from keras.layers import Add
+import keras
+import keras.backend as K
+
+from kapre.time_frequency import Melspectrogram as kapre_Melspectrogram
+
+class Melspectrogram():
+    def __init__(self, 
+                 n_dft=512, 
+                 n_hop=256, 
+                 input_shape=(1,44100), 
+                 padding='same', 
+                 sr=16000, 
+                 n_mels=128, 
+                 fmin=0.0, 
+                 fmax=8000, 
+                 power_melgram=1.0, 
+                 return_decibel_melgram=False, 
+                 trainable_fb=False, 
+                 trainable_kernel=False):
+        
+        self.n_dft=n_dft
+        if self.n_dft%2==0:
+            self.n_freq = self.n_dft/2+1
+        else:
+            self.n_freq = (self.n_dft+1)/2
+        self.n_hop=n_hop
+        self.input_shape=input_shape
+        self.padding=padding
+        self.sr=sr
+        self.n_mels=n_mels
+        self.fmin=fmin
+        self.fmax=fmax
+        self.power_melgram=float(power_melgram)
+        self.return_decibel_melgram=return_decibel_melgram
+        self.trainable_fb=trainable_fb
+        self.trainable_kernel=trainable_kernel
+
+    def __call__(self, inputs):
+        layer_shape = Model(inputs=inputs, outputs=inputs).output_shape
+        self.n_ch = layer_shape[1]
+        self.n_sample = layer_shape[2]
+        
+        kapre_model = Sequential()
+        kapre_model.add(kapre_Melspectrogram(n_dft=self.n_dft, n_hop=self.n_hop, input_shape=self.input_shape, padding=self.padding, sr=self.sr, n_mels=self.n_mels, fmin=self.fmin, fmax=self.fmax, power_melgram=self.power_melgram, return_decibel_melgram=False, trainable_fb=False, trainable_kernel=False))
+        self.w = kapre_model.get_weights()
+        
+        m = inputs
+        m = Reshape((self.n_ch,-1,1))(m)
+        m = Permute((2,1,3))(m)
+        m_re = Conv2D(self.n_dft/2+1, kernel_size=(self.n_dft,1), strides=(self.n_hop, 1), 
+                      padding=self.padding, use_bias=False, kernel_initializer=keras.initializers.Constant(value=self.w[0]), 
+                      trainable=self.trainable_kernel, name='stft_real')(m)
+        m_re = Lambda(lambda x: K.pow(x,2))(m_re)
+        m_im = Conv2D(self.n_dft/2+1, kernel_size=(self.n_dft,1), strides=(self.n_hop, 1), 
+                      padding=self.padding, use_bias=False, kernel_initializer=keras.initializers.Constant(value=self.w[1]), 
+                      trainable=self.trainable_kernel, name='stft_imag')(m)
+        m_im = Lambda(lambda x: K.pow(x,2))(m_im)
+        m = Add()([m_re,m_im])
+        m = Dense(self.n_mels, use_bias=False, kernel_initializer=keras.initializers.Constant(value=self.w[2]), trainable=self.trainable_fb, name='freq2mel')(m)
+        if self.power_melgram!=2.0:
+            m = Lambda(lambda x: K.pow(K.sqrt(x),self.power_melgram))(m)
+        m = Permute((3,1,2))(m)
+        outputs = m           
+
+        return outputs
